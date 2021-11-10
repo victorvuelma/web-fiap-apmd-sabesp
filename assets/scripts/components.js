f10cd0c2f873499e9d00b03d66334a1ab0a81890
Victor Vuelma <victorvuelma@outlook.com>
2021-11-10 20:22:12 -0300

    add system details page

---
+++ assets/scripts/components.js
@@ -0,0 +1,63 @@
+const measures = ["turbidez", "corAparente", "crl", "coliforme", "eColi"];
+
+function Month(params) {
+  const month = document.createElement("details");
+  month.classList.add("month");
+
+  month.appendChild(MonthTitle(params));
+  month.appendChild(Measures(params));
+
+  return month;
+}
+
+function MonthTitle(params) {
+  const title = document.createElement("summary");
+  title.classList.add("title");
+
+  const name = document.createElement("h4");
+  name.innerText = dayjs(params.data).format("MMMM");
+  title.appendChild(name);
+
+  measures.forEach((measure) => {
+    const measurePercent =
+      params[`${measure}Conforme`] / params[`${measure}Realizado`];
+    const percent = document.createElement("p");
+    percent.innerHTML = measurePercent.toLocaleString("pt-BR", {
+      style: "percent",
+    });
+
+    title.appendChild(percent);
+  });
+
+  return title;
+}
+
+function Measures(params) {
+  const measures = document.createElement("div");
+  measures.classList.add("measures");
+
+  measures.appendChild(MeasuresRow(params, "Exigido", "Exigidos"));
+  measures.appendChild(MeasuresRow(params, "Realizado", "Realizados"));
+  measures.appendChild(MeasuresRow(params, "Conforme", "Conformes"));
+
+  return measures;
+}
+
+function MeasuresRow(params, type, label) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+
+  const labelElement = document.createElement("h5");
+  labelElement.innerText = label;
+  row.appendChild(labelElement);
+
+  measures.forEach((measure) => {
+    const value = params[`${measure}${type}`];
+    const valueEl = document.createElement("p");
+    valueEl.innerHTML = value.toString();
+
+    row.appendChild(valueEl);
+  });
+
+  return row;
+}
--- assets/scripts/sistema.js
+++ assets/scripts/sistema.js
@@ -6,15 +6,15 @@ document.title =
   sistema +
   " - Qualidade da Água Distribuída por Sistema de Abastecimento";
 
-const body = document.querySelector("body");
-
 const h1 = document.querySelector("h1");
 h1.innerHTML = sistema;
+
+const content = document.querySelector("section.content");
 
 const url =
   "https://json-server-apmd-sabesp.herokuapp.com/params?sistema=" + sistema;
 fetch(url).then((response) =>
   response.json().then((params) => {
-    // body.appendChild(Header(params));
+    params.forEach((param) => content.appendChild(Month(param)));
   })
 );
